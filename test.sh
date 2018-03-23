d6d9e973f8114653ed6ea9c6aa3af2c8c36f6ecc
15cm <sinkerine@gmail.com>
2018-03-23 17:23:29 -0400

    [+] multiprocess test script

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Usage test.sh {src_dir}
+if [ $# -ne 1 ]; then
+   echo 'pass an arg as source dir(e.g. Translate)'
+   exit
+fi
+src_dir=$1
+log_dir="test-results"
+cmd_teml='CM.make "sources.cm"; Main.compile "../testcases/'
+
+[ -d $log_dir ] || mkdir $log_dir
+seq 1 49 | xargs -I {} -P 4 bash -c  "cd $src_dir; echo '${cmd_teml}test{}.tig\";' | sml > ../${log_dir}/{}.log"
