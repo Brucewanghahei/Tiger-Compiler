e20f51293093c98865fbca98ede647123a79d68a
15cm <sinkerine@gmail.com>
2018-04-26 23:51:08 -0400

    fix(test): may fix pwd with spaces

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 basedir="$1"
-rl_fpath=$(realpath --relative-to="$basedir" $2)
+rl_fpath=$(realpath --relative-to="$basedir" "$2")
 cd $basedir
 echo "CM.make \"sources.cm\"; Main.compile \"$rl_fpath\";" | sml
